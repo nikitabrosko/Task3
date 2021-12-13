bd274e8bf45d6ab8e396da697370cb3180c644ef
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-14 00:26:43 +0300

    Create class for event arguments

---
+++ AutomaticTelephoneStation/Phones/StartingCallEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using AutomaticTelephoneStation.PhoneNumbers;
+
+namespace AutomaticTelephoneStation.Phones
+{
+    public class StartingCallEventArgs
+    {
+        public IPhoneNumber SourcePhoneNumber { get; set; }
+        public IPhoneNumber TargetPhoneNumber { get; set; }
+
+        public StartingCallEventArgs(IPhoneNumber sourcePhoneNumber, IPhoneNumber targetPhoneNumber)
+        {
+            SourcePhoneNumber = sourcePhoneNumber;
+            TargetPhoneNumber = targetPhoneNumber;
+        }
+    }
+}
