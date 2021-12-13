3478008567d78f9dd5fd76bcb3a92b1bd62c2a76
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-14 00:26:14 +0300

    Create interface and class Subscriber

---
+++ AutomaticTelephoneStation/Subscribers/ISubscriber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AutomaticTelephoneStation.PhoneNumbers;
+using AutomaticTelephoneStation.Phones;
+using AutomaticTelephoneStation.TariffPlans;
+
+namespace AutomaticTelephoneStation.Subscribers
+{
+    public interface ISubscriber
+    {
+        string FirstName { get; }
+        string LastName { get; }
+        IPhone Phone { get; }
+    }
+}
---
+++ AutomaticTelephoneStation/Subscribers/Subscriber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutomaticTelephoneStation.PhoneNumbers;
+using AutomaticTelephoneStation.Phones;
+using AutomaticTelephoneStation.TariffPlans;
+
+namespace AutomaticTelephoneStation.Subscribers
+{
+    public class Subscriber : ISubscriber
+    {
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public IPhone Phone { get; }
+
+        public Subscriber(string firstName, string lastName, IPhone phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+        }
+    }
+}
