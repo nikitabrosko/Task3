a3b7314b2dda8726f7e1244e815fbdc5abaffd26
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-14 00:25:35 +0300

    Create interface and class Phone

---
+++ AutomaticTelephoneStation/Phones/IPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutomaticTelephoneStation.PhoneNumbers;
+using AutomaticTelephoneStation.Subscribers;
+using AutomaticTelephoneStation.TariffPlans;
+
+namespace AutomaticTelephoneStation.Phones
+{
+    public interface IPhone
+    {
+        event EventHandler<StartingCallEventArgs> StartCall;
+        IPhoneNumber PhoneNumber { get; }
+        ITariffPlan TariffPlan { get; }
+        void Call(IPhoneNumber phoneNumber);
+    }
+}
---
+++ AutomaticTelephoneStation/Phones/Phone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AutomaticTelephoneStation.PhoneNumbers;
+using AutomaticTelephoneStation.Subscribers;
+using AutomaticTelephoneStation.TariffPlans;
+
+namespace AutomaticTelephoneStation.Phones
+{
+    public class Phone : IPhone
+    {
+        public event EventHandler<StartingCallEventArgs> StartCall;
+
+        public IPhoneNumber PhoneNumber { get; }
+
+        public ITariffPlan TariffPlan { get; }
+
+        public Phone(ITariffPlan tariffPlan, IPhoneNumber phoneNumber)
+        {
+            TariffPlan = tariffPlan;
+            PhoneNumber = phoneNumber;
+        }
+
+        public void Call(IPhoneNumber phoneNumber)
+        {
+            OnStartCall(this, new StartingCallEventArgs(PhoneNumber, phoneNumber));
+        }
+
+        protected virtual void OnStartCall(object sender, StartingCallEventArgs args)
+        {
+            StartCall?.Invoke(sender, args);
+        }
+    }
+}
