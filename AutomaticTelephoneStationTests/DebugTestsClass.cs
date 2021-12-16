ef1c7af5d4cd47375b4cecb95a0340485af8f8f5
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-16 13:50:43 +0300

    Update debug code

--- AutomaticTelephoneStationTests/DebugTestsClass.cs
+++ AutomaticTelephoneStationTests/DebugTestsClass.cs
@@ -4,6 +4,7 @@ using AutomaticTelephoneStation.Ports;
 using AutomaticTelephoneStation.Stations;
 using AutomaticTelephoneStation.Subscribers;
 using AutomaticTelephoneStation.TariffPlans;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AutomaticTelephoneStationTests
@@ -14,15 +15,24 @@ namespace AutomaticTelephoneStationTests
         [TestMethod]
         public void DebugTest()
         {
-            var phone = new Phone(new MediumTariffPlan(), new PhoneNumber("6694581"));
-            var subscriber = new Subscriber("Nikita", "Brosko", phone);
-            var port = new Port();
+            var subscriber = new Subscriber
+            (
+                "Nikita", 
+                "Brosko", 
+                new Port(), 
+                new Phone
+                (
+                    new MediumTariffPlan(), 
+                    new PhoneNumber("6694581")
+                )
+            );
+
             var station = new Station();
 
-            phone.StartCall += port.OnPhoneStartingCall;
-            port.StartCall += station.OnPhoneStartingCall;
+            subscriber.Port.StartCall += station.OnPhoneStartingCall;
 
-            phone.Call(new PhoneNumber("1234567"));
+            subscriber.Phone.ConnectToPort();
+            subscriber.Phone.Call(new PhoneNumber("1234567"));
         }
     }
 }
