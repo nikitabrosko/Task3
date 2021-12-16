1ea8338670c84ff49a8a172bbc163959eca9996e
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-16 13:41:34 +0300

    Create new event args class

---
+++ AutomaticTelephoneStation/Phones/ConnectionStateEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticTelephoneStation.Phones
+{
+    public class ConnectionStateEventArgs
+    {
+        public ConnectionState ConnectionState { get; }
+
+        public ConnectionStateEventArgs(ConnectionState connectionState)
+        {
+            ConnectionState = connectionState;
+        }
+    }
+}
