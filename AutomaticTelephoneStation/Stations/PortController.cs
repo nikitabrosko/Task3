b3e972adf2e5bce92c2c08442eb6ee0a52083d5b
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-16 16:48:35 +0300

    Create a PortController class

---
+++ AutomaticTelephoneStation/Stations/PortController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AutomaticTelephoneStation.Ports;
+
+namespace AutomaticTelephoneStation.Stations
+{
+    public class PortController
+    {
+        private readonly IList<IPort> _ports = new List<IPort>();
+
+        public IEnumerable<IPort> Ports => new ReadOnlyCollection<IPort>(_ports);
+
+        public void AddPort(IPort port)
+        {
+            _ports.Add(port);
+        }
+    }
+}
