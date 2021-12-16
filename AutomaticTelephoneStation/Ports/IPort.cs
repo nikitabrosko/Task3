209867f7c2f40fc13b149231cd3fb4edc3d5c6c6
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-16 16:46:12 +0300

    Update Port entity: Change StartCall event name to OutgoingCall Add new event IncomingCall Add new event CallChangeState

--- AutomaticTelephoneStation/Ports/IPort.cs
+++ AutomaticTelephoneStation/Ports/IPort.cs
@@ -5,10 +5,14 @@ namespace AutomaticTelephoneStation.Ports
 {
     public interface IPort
     {
-        event EventHandler<StartingCallEventArgs> StartCall;
+        event EventHandler<StartingCallEventArgs> OutgoingCall;
+        event EventHandler<StationCallingEventArgs> IncomingCall;
+        public event EventHandler<StationCallingEventArgs> CallChangeState;
         PortState State { get; }
         ConnectionState ConnectionState { get; }
         void OnPhoneStartingCall(object sender, StartingCallEventArgs args);
         void OnConnectionChange(object sender, ConnectionStateEventArgs args);
+        void OnPhoneCallingByStation(object sender, StationCallingEventArgs args);
+        void OnCallChangeStateFromPhone(object sender, StationCallingEventArgs args);
     }
 }
--- AutomaticTelephoneStation/Ports/Port.cs
+++ AutomaticTelephoneStation/Ports/Port.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using AutomaticTelephoneStation.PhoneNumbers;
 using AutomaticTelephoneStation.Phones;
 
 namespace AutomaticTelephoneStation.Ports
 {
     public class Port : IPort
     {
-        public event EventHandler<StartingCallEventArgs> StartCall;
+        public event EventHandler<StartingCallEventArgs> OutgoingCall;
+        public event EventHandler<StationCallingEventArgs> IncomingCall;
+        public event EventHandler<StationCallingEventArgs> CallChangeState;
 
         public PortState State { get; private set; }
 
@@ -19,12 +22,25 @@ namespace AutomaticTelephoneStation.Ports
 
         public void OnPhoneStartingCall(object sender, StartingCallEventArgs args)
         {
-            if (State is PortState.Free)
+            if (State is PortState.Free && ConnectionState == ConnectionState.Connected)
             {
                 State = PortState.Busy;
 
-                OnStartingCall(this, new StartingCallEventArgs(args.SourcePhoneNumber, args.TargetPhoneNumber));
+                OnOutgoingCall(this, new StartingCallEventArgs(args.SourcePhoneNumber, args.TargetPhoneNumber));
             }
+
+            State = PortState.Free;
+        }
+
+        public void OnPhoneCallingByStation(object sender, StationCallingEventArgs args)
+        {
+            if (State == PortState.Free && ConnectionState == ConnectionState.Connected)
+            {
+                State = PortState.Busy;
+                OnIncomingCall(this, args);
+            }
+
+            State = PortState.Free;
         }
 
         public void OnConnectionChange(object sender, ConnectionStateEventArgs args)
@@ -32,9 +48,24 @@ namespace AutomaticTelephoneStation.Ports
             ConnectionState = args.ConnectionState;
         }
 
-        protected virtual void OnStartingCall(object senderPhone, StartingCallEventArgs args)
+        public void OnCallChangeStateFromPhone(object sender, StationCallingEventArgs args)
         {
-            StartCall?.Invoke(senderPhone, args);
+            OnCallChangeState(sender, args);
+        }
+
+        protected virtual void OnOutgoingCall(object sender, StartingCallEventArgs args)
+        {
+            OutgoingCall?.Invoke(sender, args);
+        }
+
+        protected virtual void OnIncomingCall(object sender, StationCallingEventArgs args)
+        {
+            IncomingCall?.Invoke(sender, args);
+        }
+
+        protected virtual void OnCallChangeState(object sender, StationCallingEventArgs args)
+        {
+            CallChangeState?.Invoke(sender, args);
         }
     }
 }
