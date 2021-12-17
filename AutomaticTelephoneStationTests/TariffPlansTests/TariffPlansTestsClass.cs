d5dfac58ea9858e0097843d9a86c3a77d3988d8d
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-17 17:28:05 +0300

    Create TariffPlansTests class

---
+++ AutomaticTelephoneStationTests/TariffPlansTests/TariffPlansTestsClass.cs
@@ -0,0 +1,45 @@
+﻿using AutomaticTelephoneStation.TariffPlans;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomaticTelephoneStationTests.TariffPlansTests
+{
+    [TestClass]
+    public class TariffPlansTestsClass
+    {
+        [TestMethod]
+        public void TestCreatingLowTariffPlanClass()
+        {
+            var name = "Low tariff plan";
+            var fee = 5M;
+
+            var lowTariffPlanObject = new LowTariffPlan();
+
+            Assert.IsTrue(lowTariffPlanObject.Name.Equals(name)
+                          && lowTariffPlanObject.Fee.Equals(fee));
+        }
+
+        [TestMethod]
+        public void TestCreatingMediumTariffPlanClass()
+        {
+            var name = "Medium tariff plan";
+            var fee = 10M;
+
+            var mediumTariffPlanObject = new MediumTariffPlan();
+
+            Assert.IsTrue(mediumTariffPlanObject.Name.Equals(name)
+                          && mediumTariffPlanObject.Fee.Equals(fee));
+        }
+
+        [TestMethod]
+        public void TestCreatingHighTariffPlanClass()
+        {
+            var name = "High tariff plan";
+            var fee = 15M;
+
+            var highTariffPlanObject = new HighTariffPlan();
+
+            Assert.IsTrue(highTariffPlanObject.Name.Equals(name)
+                          && highTariffPlanObject.Fee.Equals(fee));
+        }
+    }
+}
