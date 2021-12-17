975c5156ce543fd5bbdf609644998564fe7130fc
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-17 20:28:26 +0300

    Create new SubscriberTestsClass tests class Remove DebugTestsClass

---
+++ AutomaticTelephoneStationTests/SubscribersTests/SubscriberTestsClass.cs
@@ -0,0 +1,29 @@
+﻿using AutomaticTelephoneStation.PhoneNumbers;
+using AutomaticTelephoneStation.Phones;
+using AutomaticTelephoneStation.Ports;
+using AutomaticTelephoneStation.Subscribers;
+using AutomaticTelephoneStation.TariffPlans;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomaticTelephoneStationTests.SubscribersTests
+{
+    [TestClass]
+    public class SubscriberTestsClass
+    {
+        [TestMethod]
+        public void TestCreatingSubscriberClass()
+        {
+            var phoneNumberObject = new PhoneNumber("1234567");
+            var tariffPlanObject = new LowTariffPlan();
+            var phoneObject = new Phone(tariffPlanObject, phoneNumberObject);
+            var portObject = new Port(phoneObject);
+
+            var subscriberObject = new Subscriber("Nikita", "Brosko", portObject);
+
+            Assert.IsTrue(subscriberObject.FirstName.Equals("Nikita")
+                          && subscriberObject.LastName.Equals("Brosko")
+                          && subscriberObject.Phone.Equals(phoneObject)
+                          && subscriberObject.Port.Equals(portObject));
+        }
+    }
+}
