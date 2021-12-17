2ecb0b487c0a2f1f69a9303146ff4228faed6a7b
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-17 15:12:09 +0300

    Create PhoneNumberTests class

---
+++ AutomaticTelephoneStationTests/PhoneNumbersTests/PhoneNumbersTestsClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AutomaticTelephoneStation.PhoneNumbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomaticTelephoneStationTests.PhoneNumbersTests
+{
+    [TestClass]
+    public class PhoneNumbersTestsClass
+    {
+        [TestMethod]
+        public void TestCreatingPhoneNumberWithValidParameters()
+        {
+            var number = "1234567";
+
+            var phoneNumberObject = new PhoneNumber(number);
+            var expectedPhoneNumber = string.Concat("+375", "77", number);
+            var actualPhoneNumber = phoneNumberObject.Number;
+
+            Assert.AreEqual(expectedPhoneNumber, actualPhoneNumber);
+        }
+
+        [TestMethod]
+        [DataRow("12345678")]
+        [DataRow("123456")]
+        [DataRow("123")]
+        public void TestCreatingPhoneNumberWithInvalidParametersNumberLengthIsIncorrect(string number)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new PhoneNumber(number));
+        }
+    }
+}
