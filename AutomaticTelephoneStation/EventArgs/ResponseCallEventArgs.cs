cf05a89112c9d052117fdfe1433e2e3f66cb0b30
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-17 20:18:15 +0300

    Create new ResponseCallEventArgs class

---
+++ AutomaticTelephoneStation/EventArgs/ResponseCallEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using AutomaticTelephoneStation.Calls;
+
+namespace AutomaticTelephoneStation.EventArgs
+{
+    public class ResponseCallEventArgs
+    {
+        public CallState CallState { get; set; }
+
+        public ResponseCallEventArgs(CallState callState)
+        {
+            CallState = callState;
+        }
+    }
+}
