219d526cbbd90b56b6e606a8d0397439b92ad2b8
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-20 18:31:23 +0300

    Create new PhoneNumbersRepository class Create new PhoneNumbersRepositoryTests tests class

---
+++ AutomaticTelephoneStation/PhoneNumbersRepositories/PhoneNumbersRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using AutomaticTelephoneStation.PhoneNumbers;
+
+namespace AutomaticTelephoneStation.PhoneNumbersRepositories
+{
+    public class PhoneNumbersRepository
+    {
+        private readonly IList<IPhoneNumber> _phoneNumbers = new List<IPhoneNumber>();
+
+        public IEnumerable<IPhoneNumber> PhoneNumbers => new ReadOnlyCollection<IPhoneNumber>(_phoneNumbers);
+
+        public void AddNumber(IPhoneNumber phoneNumber)
+        {
+            if (CheckForExist(phoneNumber))
+            {
+                throw new ArgumentException($"Number {phoneNumber} is already exists");
+            }
+
+            _phoneNumbers.Add(phoneNumber);
+        }
+
+        public bool CheckForExist(IPhoneNumber phoneNumber)
+        {
+            return _phoneNumbers.Any(number => number.Number.Equals(phoneNumber.Number));
+        }
+    }
+}
---
+++ AutomaticTelephoneStationTests/PhoneNumbersRepositoriesTests/PhoneNumbersRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using AutomaticTelephoneStation.PhoneNumbers.OperatorCodes;
+using AutomaticTelephoneStation.PhoneNumbers.PhoneNumbers;
+using AutomaticTelephoneStation.PhoneNumbersRepositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomaticTelephoneStationTests.PhoneNumbersRepositoriesTests
+{
+    [TestClass]
+    public class PhoneNumbersRepositoryTests
+    {
+        [TestMethod]
+        public void TestAddMethod()
+        {
+            var phoneNumbersRepository = new PhoneNumbersRepository();
+            var phoneNumberObjectFirst = new BelarusPhoneNumber(BelarusOperatorCode.Mts, "1234567");
+
+            phoneNumbersRepository.AddNumber(phoneNumberObjectFirst);
+
+            Assert.IsTrue(phoneNumbersRepository.PhoneNumbers.Contains(phoneNumberObjectFirst));
+        }
+
+        [TestMethod]
+        public void TestAddMethodWithExistingNumber()
+        {
+            var phoneNumbersRepository = new PhoneNumbersRepository();
+            var phoneNumberObject = new BelarusPhoneNumber(BelarusOperatorCode.Mts, "1234567");
+
+            phoneNumbersRepository.AddNumber(phoneNumberObject);
+
+            Assert.ThrowsException<ArgumentException>(() => phoneNumbersRepository.AddNumber(phoneNumberObject));
+        }
+    }
+}
