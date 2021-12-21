c89606ee19e5aa938fa2d4405f6546b0410f1c3a
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-21 15:54:08 +0300

    Create StationReportEventArgs class

---
+++ AutomaticTelephoneStation/EventArgs/StationReportEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using AutomaticTelephoneStation.Calls;
+
+namespace AutomaticTelephoneStation.EventArgs
+{
+    public class StationReportEventArgs
+    {
+        public ICall CallReport { get; set; }
+
+        public StationReportEventArgs(ICall call)
+        {
+            CallReport = call;
+        }
+    }
+}
