eb08ba3daf2950df4543c8b22a77c06e719473fc
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-21 15:41:33 +0300

    Add an implementation for CallReport class

--- AutomaticTelephoneStation/CallReports/CallReport.cs
+++ AutomaticTelephoneStation/CallReports/CallReport.cs
@@ -1,6 +1,24 @@
-﻿namespace AutomaticTelephoneStation.CallReports
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AutomaticTelephoneStation.Calls;
+
+namespace AutomaticTelephoneStation.CallReports
 {
     public class CallReport : ICallReport
     {
+        private readonly IList<ICall> _calls = new List<ICall>();
+
+        public IEnumerable<ICall> Calls => new ReadOnlyCollection<ICall>(_calls);
+
+        public void AddCall(ICall call)
+        {
+            if (call is null)
+            {
+                throw new ArgumentNullException(nameof(call));
+            }
+
+            _calls.Add(call);
+        }
     }
 }
--- AutomaticTelephoneStation/CallReports/ICallReport.cs
+++ AutomaticTelephoneStation/CallReports/ICallReport.cs
@@ -1,6 +1,11 @@
-﻿namespace AutomaticTelephoneStation.CallReports
+﻿using System.Collections.Generic;
+using AutomaticTelephoneStation.Calls;
+
+namespace AutomaticTelephoneStation.CallReports
 {
     public interface ICallReport
     {
+        IEnumerable<ICall> Calls { get; }
+        void AddCall(ICall call);
     }
 }
