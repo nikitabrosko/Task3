32061dca4609e5a208f0ed5f8f688d153b2b8666
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-21 18:07:13 +0300

    Rework CallReportsTestsClass tests class

--- AutomaticTelephoneStationTests/CallReportsTests/CallReportsTestsClass.cs
+++ AutomaticTelephoneStationTests/CallReportsTests/CallReportsTestsClass.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Linq;
 using AutomaticTelephoneStation.CallReports;
-using AutomaticTelephoneStation.Calls;
+using AutomaticTelephoneStation.PhoneNumbers;
 using AutomaticTelephoneStation.PhoneNumbers.OperatorCodes;
 using AutomaticTelephoneStation.PhoneNumbers.PhoneNumbers;
+using AutomaticTelephoneStation.TariffPlans;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AutomaticTelephoneStationTests.CallReportsTests
@@ -12,27 +13,98 @@ namespace AutomaticTelephoneStationTests.CallReportsTests
     public class CallReportsTestsClass
     {
         [TestMethod]
-        public void TestCreatingCallReportsClassWithValidParameters()
+        public void TestCreatingCallerCallReportClassWithValidParameters()
         {
-            var callReportObject = new CallReport();
+            var callerPhoneNumberObject =
+                new BelarusPhoneNumber(BelarusOperatorCode.A1, new LowTariffPlan(), "1234567");
+            var receiverPhoneNumberObject =
+                new BelarusPhoneNumber(BelarusOperatorCode.A1, new LowTariffPlan(), "7654321");
+            var callDuration = 10;
+            var expectedFee = 50M;
 
-            var callObject = new Call(
-                new BelarusPhoneNumber(BelarusOperatorCode.Life, "1234567"),
-                new BelarusPhoneNumber(BelarusOperatorCode.Mts, "7654321"));
+            var callerCallReportObject =
+                new CallerCallReport(callerPhoneNumberObject, receiverPhoneNumberObject, callDuration);
 
-            callReportObject.AddCall(callObject);
-
-            Assert.AreEqual(callObject, callReportObject.Calls.First());
+            Assert.IsTrue(receiverPhoneNumberObject.Equals(callerCallReportObject.PhoneNumber)
+                          && callDuration.Equals(callerCallReportObject.CallDuration)
+                          && expectedFee.Equals(callerCallReportObject.Fee));
         }
 
         [TestMethod]
-        public void TestCreatingCallReportsClassWithInvalidParametersCallIsNull()
+        public void TestCreatingCallerCallReportClassWithInvalidParametersCallerPhoneNumberIsNull()
         {
-            var callReportObject = new CallReport();
+            IPhoneNumber callerPhoneNumberObject = null;
+            var receiverPhoneNumberObject =
+                new BelarusPhoneNumber(BelarusOperatorCode.A1, new LowTariffPlan(), "7654321");
+            var callDuration = 10;
+            var expectedFee = 50M;
 
-            ICall callObject = null;
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new CallerCallReport(callerPhoneNumberObject, receiverPhoneNumberObject, callDuration));
+        }
 
-            Assert.ThrowsException<ArgumentNullException>(() => callReportObject.AddCall(callObject));
+        [TestMethod]
+        public void TestCreatingCallerCallReportClassWithInvalidParametersReceiverPhoneNumberIsNull()
+        {
+            var callerPhoneNumberObject =
+                new BelarusPhoneNumber(BelarusOperatorCode.A1, new LowTariffPlan(), "1234567");
+            IPhoneNumber receiverPhoneNumberObject = null;
+            var callDuration = 10;
+            var expectedFee = 50M;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new CallerCallReport(callerPhoneNumberObject, receiverPhoneNumberObject, callDuration));
+        }
+
+        [TestMethod]
+        public void TestCreatingReceiverCallReportClassWithValidParameters()
+        {
+            var callerPhoneNumberObject =
+                new BelarusPhoneNumber(BelarusOperatorCode.A1, new LowTariffPlan(), "1234567");
+            var callDuration = 10;
+
+            var receiverCallReportObject =
+                new ReceiverCallReport(callerPhoneNumberObject, callDuration);
+
+            Assert.IsTrue(callerPhoneNumberObject.Equals(receiverCallReportObject.PhoneNumber)
+                          && callDuration.Equals(receiverCallReportObject.CallDuration));
+        }
+
+        [TestMethod]
+        public void TestCreatingReceiverCallReportClassWithInvalidParametersCallerPhoneNumberIsNull()
+        {
+            IPhoneNumber callerPhoneNumberObject = null;
+            var callDuration = 10;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                new ReceiverCallReport(callerPhoneNumberObject, callDuration));
+        }
+
+        [TestMethod]
+        public void TestCreatingCallReportsRepositoryClassWithValidParameters()
+        {
+            var callReportRepositoryObject = new CallReportRepository();
+
+            var callerPhoneNumberObject =
+                new BelarusPhoneNumber(BelarusOperatorCode.A1, new LowTariffPlan(), "1234567");
+            var callDuration = 10;
+
+            var receiverCallReportObject =
+                new ReceiverCallReport(callerPhoneNumberObject, callDuration);
+
+            callReportRepositoryObject.AddCall(receiverCallReportObject);
+
+            Assert.AreEqual(receiverCallReportObject, callReportRepositoryObject.Calls.First());
+        }
+
+        [TestMethod]
+        public void TestCreatingCallReportsRepositoryClassWithInvalidParametersCallReportIsNull()
+        {
+            var callReportRepositoryObject = new CallReportRepository();
+
+            ICallReport callObject = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => callReportRepositoryObject.AddCall(callObject));
         }
     }
 }
