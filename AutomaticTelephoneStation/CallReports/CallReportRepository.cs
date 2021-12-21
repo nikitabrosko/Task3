aa8fcb5096431a3e499f212e82712d623de69691
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-21 17:59:23 +0300

    Add CallReportRepository class Add ICallReportRepository interface

---
+++ AutomaticTelephoneStation/CallReports/CallReportRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace AutomaticTelephoneStation.CallReports
+{
+    public class CallReportRepository : ICallReportRepository
+    {
+        private readonly IList<ICallReport> _calls = new List<ICallReport>();
+
+        public IEnumerable<ICallReport> Calls => new ReadOnlyCollection<ICallReport>(_calls);
+
+        public void AddCall(ICallReport callReport)
+        {
+            if (callReport is null)
+            {
+                throw new ArgumentNullException(nameof(callReport));
+            }
+
+            _calls.Add(callReport);
+        }
+    }
+}
---
+++ AutomaticTelephoneStation/CallReports/ICallReportRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using AutomaticTelephoneStation.Calls;
+
+namespace AutomaticTelephoneStation.CallReports
+{
+    public interface ICallReportRepository
+    {
+        IEnumerable<ICallReport> Calls { get; }
+        void AddCall(ICallReport call);
+    }
+}
